b2d47e57523b3c8400317c84092f51c4f16cac4a
jeffreybiles <bilesjeffrey@gmail.com>
2015-02-28 17:35:17 -0600

    faaaaancy sort arrows

--- app/components/sortable-header.js
+++ app/components/sortable-header.js
@@ -2,12 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'th',
-  downArrow: '&#9660',
-  upArrow: '&#9650',
+  downArrow: Ember.computed('isAscending', 'isCurrentSort', function(){
+    if(this.get('isCurrentSort') && this.get('isAscending')){
+      return '<span class="gold">&#9660<span>';
+    } else {
+      return '&#9660';
+    }
+  }),
+  upArrow: Ember.computed('isAscending', 'isCurrentSort', function(){
+    if(this.get('isCurrentSort') && !this.get('isAscending')){
+      return '<span class="gold">&#9650</span>';
+    } else {
+      return '&#9650';
+    }
+  }),
 
+  isCurrentSort: Ember.computed('parent.sortProperties.firstObject', 'sortProperty', function(){
+    return this.get('parent.sortProperties.firstObject') === this.get('sortProperty');
+  }),
+  isAscending: Ember.computed.alias('parent.sortAscending'),
   actions: {
     sortBy: function(order){
-      this.sendAction('action', this.get('sortProperty'), order)
+      this.sendAction('action', this.get('sortProperty'), order);
     }
   }
 });
