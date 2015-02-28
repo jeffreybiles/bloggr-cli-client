b6fe7da66af8607fdd4fa7005bb36d3477288e88
jeffreybiles <bilesjeffrey@gmail.com>
2015-02-28 16:22:34 -0600

    all the sorting magic.  Currently lacks visual indicators.

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['createdAt'],
+  sortAscending: true,
+  actions: {
+    sortBy: function(property){
+      if(property = this.get('sortProperties')[0]){
+        this.toggleProperty('sortAscending')
+      } else {
+        this.set('sortAscending', true)
+      }
+      this.set('sortProperties', [property]);
+    }
+  }
 });
