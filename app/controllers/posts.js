af1c310f3fc890321d77caf5e2febcca5f31d1a2
jeffreybiles <bilesjeffrey@gmail.com>
2015-02-28 22:31:49 -0600

    lol so easy

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  queryParams: ['sortProperties', 'sortAscending'],
   sortProperties: ['createdAt'],
   sortAscending: true,
   actions: {
