d94b1a2b04e0e18679b079fea2fd39d99300663f
jeffreybiles <bilesjeffrey@gmail.com>
2015-03-20 00:45:52 -0500

    finished product

--- app/components/sortable-header.js
+++ app/components/sortable-header.js
@@ -5,6 +5,16 @@ export default Ember.Component.extend({
   downArrow: '&#9660',
   upArrow: '&#9650',
 
+  isSelectedSort: Ember.computed('selectedSorts', 'sortProperty', function(){
+    return this.get('selectedSorts')[0] === this.get('sortProperty');
+  }),
+  upArrowHighlighted: Ember.computed('isSelectedSort', 'isAscending', function(){
+    return this.get("isSelectedSort") && !this.get('isAscending');
+  }),
+  downArrowHighlighted: Ember.computed('isSelectedSort', 'isAscending', function(){
+    return this.get("isSelectedSort") && this.get('isAscending');
+  }),
+  
   actions: {
     sortBy: function(order){
       this.sendAction('action', this.get('sortProperty'), order)
