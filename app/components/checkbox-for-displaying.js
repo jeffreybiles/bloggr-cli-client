0f2b8c6d53aacbc81d32d38b99f02d1e5a827bdc
jeffreybiles <bilesjeffrey@gmail.com>
2015-03-22 04:43:52 -0500

    Holy Macaroni, it works!  Rearrange columns, save it in the URL!!

--- app/components/checkbox-for-displaying.js
+++ app/components/checkbox-for-displaying.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'span',
   isDisplayed: Ember.computed('displayableItem.property', 'displayedList.@each', function(){
     var displayedList = this.get('displayedList')
     return displayedList.contains(this.get('displayableItem.property'))
   }),
   actions: {
     setBoolean: function(){
-      console.log(this.get('isDisplayed'), this.get("displayedList"))
       if(this.get('isDisplayed')){
         this.get('displayedList').removeObject(this.get('displayableItem.property'))
       } else {
-        this.get('displayedList').pushObject(this.get('displayableItem.property'))
+        this.get('displayedList').addObject(this.get('displayableItem.property'))
       }
     }
   }
