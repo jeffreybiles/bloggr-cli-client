f2e4f79e7c7e29f9700ec9e23f63b438086a39d6
jeffreybiles <bilesjeffrey@gmail.com>
2015-04-04 13:19:53 -0500

    pagination and clickable page numbers

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend(Ember.SortableMixin, {
-  queryParams: ['sortProperties', 'sortAscending'],
+  queryParams: ['sortProperties', 'sortAscending', 'pageNumber'],
   sortProperties: ['createdAt'],
-  sortAscending: true
+  sortAscending: true,
+
+  pageNumber: 0,
+  pageSize: 10,
+
+  pages: Ember.computed('arrangedContent', 'pageSize', 'sortAscending', function(){
+    var pages = [];
+    var arrangedContent = this.get('arrangedContent').copy();
+    while (arrangedContent.length > 0) {
+      pages.push(arrangedContent.splice(0, this.get('pageSize')));
+    }
+    return pages;
+  }),
+  paginatedContent: Ember.computed('pages', 'pageNumber', function(){
+    return this.get('pages')[this.get('pageNumber')]
+  }),
 });
