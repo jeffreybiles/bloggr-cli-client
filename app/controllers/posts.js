dcffa6e356cfe9d9758e744da0bbe08b08ba594e
jeffreybiles <bilesjeffrey@gmail.com>
2016-07-07 20:47:30 -0500

    Replace SortableMixin

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend(Ember.SortableMixin, {
+export default Ember.Controller.extend({
   queryParams: ['sortProperties', 'sortAscending', 'pageNumber', 'pageSize', 'columnsUsed'],
   sortProperties: ['createdAt'],
   sortAscending: true,
+  arrangedContent: Ember.computed('model', 'sortProperties', 'sortAscending', function(){
+    return this.get('model').toArray().sort((a, b)=>{
+      let sortProperty = this.get('sortProperties')[0];
+      if(this.get('sortAscending')){
+        return Ember.compare(a.get(sortProperty), b.get(sortProperty));
+      } else {
+        return Ember.compare(b.get(sortProperty), a.get(sortProperty));
+      }
+    })
+  }),
 
   availableColumns: [
     {'title': 'Title', 'property': 'title', 'display': 'plain'},
